0f1e84735f51332ace0cd7893029d0b0897d36bf
Jeriel Baptista Verissimo <jeriel.verissimo@outlook.com>
2017-03-10 12:46:04 -0300

    Começando a projetar o layout

--- Pizzaria/Form1.Designer.cs
+++ Pizzaria/Form1.Designer.cs
@@ -28,20 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panTitleBar = new System.Windows.Forms.Panel();
+            this.panPrincipal = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.panActionBar = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panTitleBar.SuspendLayout();
+            this.panPrincipal.SuspendLayout();
+            this.panActionBar.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panTitleBar
+            // 
+            this.panTitleBar.BackColor = System.Drawing.Color.SandyBrown;
+            this.panTitleBar.Controls.Add(this.lblTitle);
+            this.panTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panTitleBar.Location = new System.Drawing.Point(0, 0);
+            this.panTitleBar.Margin = new System.Windows.Forms.Padding(0);
+            this.panTitleBar.Name = "panTitleBar";
+            this.panTitleBar.Size = new System.Drawing.Size(784, 25);
+            this.panTitleBar.TabIndex = 0;
+            this.panTitleBar.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.panTitleBar_MouseDoubleClick);
+            this.panTitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panTitleBar_MouseDown);
+            this.panTitleBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panTitleBar_MouseMove);
+            // 
+            // panPrincipal
+            // 
+            this.panPrincipal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panPrincipal.Controls.Add(this.panActionBar);
+            this.panPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panPrincipal.Location = new System.Drawing.Point(0, 25);
+            this.panPrincipal.Name = "panPrincipal";
+            this.panPrincipal.Size = new System.Drawing.Size(784, 537);
+            this.panPrincipal.TabIndex = 1;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(347, 3);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(91, 19);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Pizzaria C#";
+            // 
+            // panActionBar
+            // 
+            this.panActionBar.BackColor = System.Drawing.Color.Gold;
+            this.panActionBar.Controls.Add(this.label1);
+            this.panActionBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panActionBar.Location = new System.Drawing.Point(0, 0);
+            this.panActionBar.Margin = new System.Windows.Forms.Padding(0);
+            this.panActionBar.Name = "panActionBar";
+            this.panActionBar.Size = new System.Drawing.Size(784, 75);
+            this.panActionBar.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(60, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pizzaria C#";
             // 
             // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.panPrincipal);
+            this.Controls.Add(this.panTitleBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Pizzaria C#";
+            this.panTitleBar.ResumeLayout(false);
+            this.panTitleBar.PerformLayout();
+            this.panPrincipal.ResumeLayout(false);
+            this.panActionBar.ResumeLayout(false);
+            this.panActionBar.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panTitleBar;
+        private System.Windows.Forms.Panel panPrincipal;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Panel panActionBar;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Pizzaria/Form1.cs
+++ Pizzaria/Form1.cs
@@ -12,9 +12,62 @@ namespace Pizzaria
 {
     public partial class frmPrincipal : Form
     {
+
+        // variaveis globais
+        int X = 0, Y = 0;
+
         public frmPrincipal()
         {
             InitializeComponent();
+
+            setComponents();
+            
         }
+
+        void setComponents()
+        {
+            // mover formulário
+            this.MouseDown += new MouseEventHandler(panTitleBar_MouseDown);
+            this.MouseMove += new MouseEventHandler(panTitleBar_MouseMove);
+
+            // setar cores
+            panTitleBar.BackColor = ColorTranslator.FromHtml("#FBC02D");
+            panPrincipal.BackColor = ColorTranslator.FromHtml("#FFF9C4");
+            panActionBar.BackColor = ColorTranslator.FromHtml("#FFEE58");
+
+            
+        }
+
+        private void panTitleBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) return;
+            X = this.Left - MousePosition.X;
+            Y = this.Top - MousePosition.Y;
+        }
+
+        private void panTitleBar_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)  this.WindowState = FormWindowState.Maximized; 
+            else this.WindowState = FormWindowState.Normal;
+        }
+
+        private void panTitleBar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) return;
+            this.Left = X + MousePosition.X;
+            this.Top = Y + MousePosition.Y;
+        }
+
+        /*protected override CreateParams CreateParams
+        {
+            get
+            {
+                const int CS_DROPSHADOW = 0x20000;
+                CreateParams cp = base.CreateParams;
+                cp.ClassStyle |= CS_DROPSHADOW;
+                return cp;
+            }
+
+        }*/
     }
 }
